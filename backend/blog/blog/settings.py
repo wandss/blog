d679d94d1004c1bd4d44448092ed405adf25cb7f
Wanderley <wanderley@ibm.com>
2021-08-18 16:06:12 -0300

    add psycopg2 to requirements

--- backend/blog/blog/settings.py
+++ backend/blog/blog/settings.py
@@ -76,11 +76,26 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.{}'.format(
+                os.getenv('DATABASE_ENGINE', 'postgresql')
+            ),
+            'NAME': os.getenv('POSTGRESQL_DATABASE', 'db.sqlite3' ),
+            'USER': os.getenv('POSTGRESQL_USER', 'wand'),
+            'PASSWORD': os.getenv('POSTGRESQL_PASSWORD', 'youshouldchangethis'),
+            'HOST': os.getenv('DATABASE_SERVICE_NAME', 'ADDTHEDBCONTAINERNAME'),
+            'PORT': os.getenv('DATABASE_PORT', 5432),
+            'OPTIONS': json.loads(
+                os.getenv('DATABASE_OPTIONS', '{}')
+            ),
+        }
 }
 
 
